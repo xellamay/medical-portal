b1d7885dccf8aa2e3416fa03ded48ab32a3187e5
Alena <xellatroy@gmail.com>
2023-06-21 21:02:56 +0300

    сделана карточка новости и моки

---
+++ src/components/newsCard/NewsCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Elevation } from "@blueprintjs/core";
+import Text from "../text/Text";
+import classes from "./NewsCard.module.scss"
+
+interface Props {
+  id: string,
+  title: string,
+  type: string,
+  image: string,
+}
+
+const NewsCard: React.FC<Props> = ({ image, title, type, id }) => (
+  <Card className={classes.card} interactive={true} elevation={Elevation.ONE}>
+    <div>
+      <img className={classes.card__image} src={image} alt={title} />
+    </div>
+    <div className={classes.card__desc}>
+      <Text className={classes.card__chapter} tagName="div" large>
+        {type}
+      </Text>
+      <Text tagName="h3" heading>
+        {title}
+      </Text>
+    </div>
+  </Card>
+)
+
+export default NewsCard;
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import NewsCard from "../../components/newsCard/NewsCard";
 
 const HomePage: React.FC = () => (
-  <>
-    Hello!!
-  </>
+  //это для верстки для визуализации, потом здесь не будет этого
+  <NewsCard
+    id="1"
+    title="Рана на ноге - от небольшой царапины до язвы"
+    type="Медицина"
+    image="https://cs13.pikabu.ru/post_img/big/2019/12/30/1/157766020712271796.jpg"
+  />
 )
 
 export default HomePage;
