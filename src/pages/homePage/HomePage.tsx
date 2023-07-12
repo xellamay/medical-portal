4d98578a3c210bc6fc018cb0280de1ff46268fa6
unknown <xellatroy@gmail.com>
2023-07-12 14:53:36 +0300

    правка, удалила лишнее

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Slider from "../../components/slider/Slider";
 import { slideFixtures } from "../../fixtures/slide";
-import EmployeeCard from "../../components/employeeCard/EmployeeCard";
 
 const HomePage: React.FC = () => (
   <Slider data={slideFixtures} />
