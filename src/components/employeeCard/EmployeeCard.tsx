b303f3c3bdd8fde2bb1d4628e0cc26d9ce3fec4a
unknown <xellatroy@gmail.com>
2023-07-18 11:40:41 +0300

    правки после ревью

--- src/components/employeeCard/EmployeeCard.tsx
+++ src/components/employeeCard/EmployeeCard.tsx
@@ -4,28 +4,32 @@ import Text from "../text/Text";
 import classes from "./EmployeeCard.module.scss"
 
 interface Props {
-    name: string,
-    lastname: string,
-    surname: string,
-    birthday?: string,
-    image: string,
-    jobTitle: string,
-    filial: string,
-    onClick?: () => void,
+  name: string,
+  lastname: string,
+  surname: string,
+  birthday?: string,
+  image: string,
+  jobTitle: string,
+  filial: string,
+  onClick?: () => void,
 }
 
-const EmployeeCard: React.FC<Props> = ({ onClick, name, surname, lastname, birthday, filial, jobTitle, image }) => {
-    return (
-        <Card className={classes.employee} interactive={true} onClick={onClick}>
-            <img className={classes.employee__img} src={image} alt={name}/>
-            <div className={classes.employee__name}>
-                <Text tagName="div">{lastname}</Text>
-                <Text tagName="div">{name}</Text>
-                <Text tagName="div">{surname}</Text>
-                <Text tagName="div">{`${filial}, ${jobTitle}`}</Text>
-            </div>
-        </Card>
-    )
-}
+const EmployeeCard: React.FC<Props> = ({
+  onClick,
+  name, surname,
+  lastname, birthday,
+  filial, jobTitle,
+  image
+}) => (
+  <Card className={classes.employee} interactive={true} onClick={onClick}>
+    <img className={classes.employee__img} src={image} alt={name}/>
+    <div className={classes.employee__name}>
+      <Text tagName="div">{lastname}</Text>
+      <Text tagName="div">{name}</Text>
+      <Text tagName="div">{surname}</Text>
+      <Text tagName="div">{`${filial}, ${jobTitle}`}</Text>
+    </div>
+  </Card>
+)
 
 export default EmployeeCard;
